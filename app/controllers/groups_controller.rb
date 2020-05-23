fde0d07ebcc6d8a02f8d32c3b6646a9fd9e8ed4e
EC2 Default User <ec2-user@ip-172-31-39-172.ap-northeast-1.compute.internal>
2020-05-23 06:02:33 +0000

    group-edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,7 +28,7 @@ class GroupsController < ApplicationController
   
   private
   def group_params
-    params.require(:group).permit(:name, :user_id [])
+    params.require(:group).permit(:name, :user_id )
   end
   
   def grouppost_params
